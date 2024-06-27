57967f116ccac63aaa83604efd0dac23637e68c4
Negativ212 <klimovnikita132@gmail.com>
2024-06-27 21:31:25 +0800

    Hero
    полностью сделано передвижение героя

---
+++ Game_2d/Assets/Scripts/Hero.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float jumpForce = 15f;
+    private bool isGrounded = false;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private States State
+    {
+        get { return (States)anim.GetInteger("state"); }
+        set { anim.SetInteger("state", (int)value); }
+    }
+    private void Awake()
+    {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Run()
+    {
+        if (isGrounded) State = States.run;
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+        sprite.flipX = dir.x < 0.0f;
+    }
+    private void Update()
+    {
+        if (isGrounded) State = States.idle;
+        if (Input.GetButton("Horizontal"))
+            Run();
+        if (isGrounded && Input.GetButtonDown("Jump"))
+            Jump();
+        CheckGround();
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        isGrounded = collider.Length > 1;
+        if (!isGrounded) State = States.jump;
+    }
+
+    public enum States
+    {
+        idle,
+        run,
+        jump
+    }
+}
+
+
+
